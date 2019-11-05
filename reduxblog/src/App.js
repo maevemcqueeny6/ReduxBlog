24fd209835430e10a2586f67d7ff0b9b14264984
Maeve McQueeny <mamcqueeny@wsits.com>
2019-11-05 14:46:36 -0500

    adding software for redux

--- reduxblog/src/App.js
+++ reduxblog/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react'
 import './App.css';
+import { Provider } from 'react-redux';
+
 import Posts from './components/Posts';
 import PostForm from './components/PostForm'
+
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
         <h1>React Redux App</h1>
         <PostForm/>
         <Posts />
       </div>
+      </Provider>
     )
   }
 }
